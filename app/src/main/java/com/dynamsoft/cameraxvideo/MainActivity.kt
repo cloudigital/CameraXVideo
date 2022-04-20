1c5a9beafbb6da4210e2c1761c73ecec5bafd304
xulihang <xulihanghai@163.com>
2022-04-20 18:05:51 +0800

    add camera activity; add permissions

--- app/src/main/java/com/dynamsoft/cameraxvideo/MainActivity.kt
+++ app/src/main/java/com/dynamsoft/cameraxvideo/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.dynamsoft.cameraxvideo
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var PERMISSIONS_REQUIRED = arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.RECORD_AUDIO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var btn = findViewById<Button>(R.id.startRecordingButton)
+        btn.setOnClickListener {
+            var intent = Intent(this,CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        // add the storage access permission request for Android 9 and below.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            val permissionList = PERMISSIONS_REQUIRED.toMutableList()
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            PERMISSIONS_REQUIRED = permissionList.toTypedArray()
+        }
+
+        if (!hasPermissions(this)) {
+            // Request camera-related permissions
+            activityResultLauncher.launch(PERMISSIONS_REQUIRED)
+        }
+
+
     }
+
+    fun hasPermissions(context: Context) = PERMISSIONS_REQUIRED.all {
+        ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions())
+        { permissions ->
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                if (it.key in PERMISSIONS_REQUIRED && it.value == false)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(this, "Permission request denied", Toast.LENGTH_LONG).show()
+            }
+        }
 }
