f1395e7e13f1efb7f6a7a1ba14bbe2cdffaba560
xulihang <xulihanghai@163.com>
2022-04-21 14:10:30 +0800

    implement video recording with camerax

--- app/src/main/java/com/dynamsoft/cameraxvideo/CameraActivity.kt
+++ app/src/main/java/com/dynamsoft/cameraxvideo/CameraActivity.kt
@@ -1,11 +1,142 @@
 package com.dynamsoft.cameraxvideo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.*
+import androidx.camera.view.PreviewView
+import androidx.concurrent.futures.await
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.updateLayoutParams
+import com.google.common.util.concurrent.ListenableFuture
+import kotlinx.coroutines.runBlocking
+import java.text.SimpleDateFormat
+import java.util.*
+import androidx.core.util.Consumer
 
 class CameraActivity : AppCompatActivity() {
+    private var currentRecording: Recording? = null
+    private val FILENAMEFORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private val mainThreadExecutor by lazy { ContextCompat.getMainExecutor(this) }
+    private var audioEnabled = false
+    private lateinit var videoCapture: VideoCapture<Recorder>
+    private lateinit var context: Context;
+    private lateinit var durationTextView: TextView;
+
+    private var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        context = this;
+        durationTextView = findViewById<TextView>(R.id.durationTextView)
+        val decorView: View = window.decorView
+        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        runBlocking {
+            bindCaptureUsecase()
+        }
+    }
+
+    private suspend fun bindCaptureUsecase() {
+        val cameraProvider = ProcessCameraProvider.getInstance(this).await()
+
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+        // create the user required QualitySelector (video resolution): we know this is
+        // supported, a valid qualitySelector will be created.
+        var quality = Quality.HD
+        val qualitySelector = QualitySelector.from(quality)
+
+        var previewView = findViewById<PreviewView>(R.id.previewView)
+        previewView.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            dimensionRatio = "V,9:16"
+        }
+
+        val preview = Preview.Builder()
+            .setTargetAspectRatio(AspectRatio.RATIO_16_9)
+            .build().apply {
+                setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+        // build a recorder, which can:
+        //   - record video/audio to MediaStore(only shown here), File, ParcelFileDescriptor
+        //   - be used create recording(s) (the recording performs recording)
+        val recorder = Recorder.Builder()
+            .setQualitySelector(qualitySelector)
+            .build()
+        videoCapture = VideoCapture.withOutput(recorder)
+
+        try {
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(
+                this,
+                cameraSelector,
+                videoCapture,
+                preview
+            )
+            startRecording();
+        } catch (exc: Exception) {
+            exc.printStackTrace()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startRecording() {
+        // create MediaStoreOutputOptions for our recorder: resulting our recording!
+        val name = "CameraX-recording-" +
+                SimpleDateFormat(FILENAMEFORMAT, Locale.US)
+                    .format(System.currentTimeMillis()) + ".mp4"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Video.Media.DISPLAY_NAME, name)
+        }
+        val mediaStoreOutput = MediaStoreOutputOptions.Builder(
+            this.contentResolver,
+            MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+            .setContentValues(contentValues)
+            .build()
+
+        // configure Recorder and Start recording to the mediaStoreOutput.
+        currentRecording = videoCapture.output
+            .prepareRecording(this, mediaStoreOutput)
+            .apply { if (audioEnabled) withAudioEnabled() }
+            .start(mainThreadExecutor, captureListener)
+    }
+
+    /**
+     * CaptureEvent listener.
+     */
+    private val captureListener = Consumer<VideoRecordEvent> { event ->
+        val durationInNanos: Long = event.recordingStats.recordedDurationNanos;
+        val durationInSeconds: Double = durationInNanos/1000/1000/1000.0
+        Log.d("DBR","duration: "+durationInNanos)
+        durationTextView.setText("Duration: "+durationInSeconds)
+        if (durationInSeconds >= 10) {
+            Log.d("DBR","> 10 seconds")
+            if (currentRecording != null) {
+                currentRecording!!.stop();
+                Toast.makeText(context, "Saved", Toast.LENGTH_LONG).show()
+                goBack();
+            }
+        }
+    }
+
+    private fun goBack(){
+        this.onBackPressed()
     }
 }
