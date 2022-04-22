13b37afd86ec6f132320f8a81b5cabc34c302db0
xulihang <xulihanghai@163.com>
2022-04-22 17:41:14 +0800

    update license

--- app/src/main/java/com/dynamsoft/cameraxvideo/VideoActivity.kt
+++ app/src/main/java/com/dynamsoft/cameraxvideo/VideoActivity.kt
@@ -75,7 +75,7 @@ class VideoActivity : AppCompatActivity() {
     }
 
     private fun initDBR(){
-        BarcodeReader.initLicense("DLS2eyJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSJ9"
+        BarcodeReader.initLicense("DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ=="
         ) { isSuccessful, e ->
             if (!isSuccessful) {
                 e.printStackTrace()
