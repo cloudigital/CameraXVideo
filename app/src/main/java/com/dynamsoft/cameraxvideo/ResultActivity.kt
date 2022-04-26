053ba9d48ec54476e4d34e7c5b62cecc0a7182d1
xulihang <xulihanghai@163.com>
2022-04-26 17:53:14 +0800

    update webview settings

--- app/src/main/java/com/dynamsoft/cameraxvideo/ResultActivity.kt
+++ app/src/main/java/com/dynamsoft/cameraxvideo/ResultActivity.kt
@@ -1,8 +1,11 @@
 package com.dynamsoft.cameraxvideo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.webkit.WebView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var webView: WebView
@@ -10,16 +13,21 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
         webView = findViewById(R.id.webView)
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.allowFileAccess = true
         loadHTMLFromAssets();
     }
     private fun loadHTMLFromAssets(){
 
-        var param = ""
+        var param = "?time="+System.currentTimeMillis()
         if (intent.hasExtra("filename")) {
             val fileName = intent.getStringExtra("filename")
             val externalFilesPath = getExternalFilesDir("")?.absolutePath
             var outputPath = externalFilesPath + "/" + fileName
-            param = "?path="+outputPath
+            param = param+"&path="+outputPath
+            Log.d("DBR",param)
+            Toast.makeText(this,param,Toast.LENGTH_LONG).show()
         }
         webView.loadUrl("file:android_asset/index.html"+param);
     }
