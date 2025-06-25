3d46b33c7b94e87d72c5f5391f7ebfd068cc4314
cloudigital <58320678+cloudigital@users.noreply.github.com>
2025-06-25 11:24:02 +0700

    Update StealthRecordActivity.kt

--- app/src/main/java/com/dynamsoft/cameraxvideo/StealthRecordActivity.kt
+++ app/src/main/java/com/dynamsoft/cameraxvideo/StealthRecordActivity.kt
@@ -5,8 +5,9 @@ import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.os.*
 import android.view.KeyEvent
+import android.view.WindowManager
 import android.widget.Toast
-import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.CameraSelector
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.*
@@ -15,7 +16,7 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
-class StealthRecordActivity : ComponentActivity() {
+class StealthRecordActivity : AppCompatActivity() {
 
     private lateinit var cameraSelector: CameraSelector
     private lateinit var videoCapture: VideoCapture<Recorder>
@@ -23,26 +24,21 @@ class StealthRecordActivity : ComponentActivity() {
     private var isRecording = false
     private var lensFacing = CameraSelector.LENS_FACING_BACK
     private var saveGallery = true
-    
+
     private lateinit var berlinClock: BerlinClockView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        //setContentView(R.layout.activity_stealth_record)
 
-        //--------------
         // Fullscreen flags
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-        actionBar?.hide()
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
         berlinClock = BerlinClockView(this, null)
         setContentView(berlinClock)
-        //--------------
-        //berlinClock = findViewById(R.id.berlinClock)
 
         berlinClock.onToggleRecord = {
             toggleRecording()
@@ -84,28 +80,21 @@ class StealthRecordActivity : ComponentActivity() {
     }
 
     private fun toggleRecording() {
-        
-        
-        //val recordButton = findViewById<ImageButton>(R.id.btn_record)
         if (isRecording) {
             recording?.stop()
             isRecording = false
-            //recordButton.setBackgroundColor(getColor(android.R.color.holo_green_dark))
             Toast.makeText(this, "Stopped", Toast.LENGTH_SHORT).show()
         } else {
             val filename = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.US)
                 .format(System.currentTimeMillis()) + ".mp4"
-            
-            //Ghi ra thư mục riêng của app
+
             var publicDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES)
-            // Ghi ra Galery 
-            if (saveGallery) 
+            if (saveGallery)
                 publicDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
-            
-            
+
             val outputFile = File(publicDir, "CameraX/$filename")
             outputFile.parentFile?.mkdirs()
-        
+
             val outputOptions = FileOutputOptions.Builder(outputFile).build()
 
             recording = videoCapture.output
@@ -113,22 +102,17 @@ class StealthRecordActivity : ComponentActivity() {
                 .withAudioEnabled()
                 .start(ContextCompat.getMainExecutor(this)) { event ->
                     if (event is VideoRecordEvent.Finalize) {
-                        //Toast.makeText(this, "Saved: ${outputFile.absolutePath}", Toast.LENGTH_SHORT).show()
-
-                        if (saveGallery){
-                            // Gửi MediaScanner để hiện trong Gallery
+                        if (saveGallery) {
                             val uri = Uri.fromFile(outputFile)
                             val scanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE).apply {
                                 data = uri
                             }
                             sendBroadcast(scanIntent)
                         }
-                        
                     }
                 }
 
             isRecording = true
-            //recordButton.setBackgroundColor(getColor(android.R.color.holo_red_dark))
             Toast.makeText(this, "Timeup !", Toast.LENGTH_SHORT).show()
         }
     }
